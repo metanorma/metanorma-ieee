6b5e5860f621f4f590bd73e962564b516884b832
Nick Nicholas <opoudjis@gmail.com>
2022-06-12 19:34:46 +1000

    init

--- lib/metanorma/ieee/validate.rb
+++ lib/metanorma/ieee/validate.rb
@@ -39,6 +39,98 @@ module Metanorma
         ret = "Trial-Use" if ret == "Trial Use"
         ret
       end
+
+      def section_validate(doc)
+        doctype = doc&.at("//bibdata/ext/doctype")&.text
+        unless %w(amendment technical-corrigendum).include? doctype
+          sections_presence_validate(doc.root)
+          sections_sequence_validate(doc.root)
+        end
+        super
+      end
+
+      def sections_presence_validate(root)
+        root.at("//sections/clause[@type = 'overview']") or
+          @log.add("Style", nil, "Overview clause missing")
+        root.at("//sections/clause[@type = 'overview']/clause[@type = 'scope']") or
+          @log.add("Style", nil, "Scope subclause missing")
+        root.at("//sections/clause[@type = 'overview']/clause[@type = 'word-usage']") or
+          @log.add("Style", nil, "Word Usage subclause missing")
+        root.at("//references[@normative = 'true']") or
+          @log.add("Style", nil, "Normative references missing")
+        root.at("//terms") or
+          @log.add("Style", nil, "Definitions missing")
+      end
+
+      def seqcheck(names, msg, accepted)
+        n = names.shift
+        return [] if n.nil?
+
+        test = accepted.map { |a| n.at(a) }
+        if test.all?(&:nil?)
+          @log.add("Style", nil, msg)
+        end
+        names
+      end
+
+      # spec of permissible section sequence
+      # we skip normative references, it goes to end of list
+      SEQ = [
+        { msg: "Initial section must be (content) Abstract",
+          val: ["./self::abstract"] },
+        { msg: "Prefatory material must be followed by (clause) Overview",
+          val: ["./self::clause[@type = 'overview']"] },
+        { msg: "Normative References must be followed by "\
+               "Definitions",
+          val: ["./self::terms | .//terms"] },
+      ].freeze
+
+      SECTIONS_XPATH =
+        "//preface/abstract | //sections/terms | .//annex | "\
+        "//sections/definitions | //sections/clause | "\
+        "//references[not(parent::clause)] | "\
+        "//clause[descendant::references][not(parent::clause)]".freeze
+
+      def sections_sequence_validate(root)
+        names, n = sections_sequence_validate_start(root)
+        names, n = sections_sequence_validate_body(names, n)
+        sections_sequence_validate_end(names, n)
+      end
+
+      def sections_sequence_validate_start(root)
+        names = root.xpath(SECTIONS_XPATH)
+        names = seqcheck(names, SEQ[0][:msg], SEQ[0][:val])
+        n = names[0]
+        names = seqcheck(names, SEQ[1][:msg], SEQ[1][:val])
+        names = seqcheck(names, SEQ[2][:msg], SEQ[2][:val])
+        n = names.shift
+        n = names.shift if n&.at("./self::definitions")
+        [names, n]
+      end
+
+      def sections_sequence_validate_body(names, elem)
+        [names, elem]
+      end
+
+      def sections_sequence_validate_end(names, elem)
+        while elem&.name == "annex"
+          elem = names.shift
+          if elem.nil?
+            @log.add("Style", nil, "Document must include (references) "\
+                                   "Normative References")
+          end
+        end
+        elem&.at("./self::references[@normative = 'true']") ||
+          @log.add("Style", nil, "Document must include (references) "\
+                                 "Normative References")
+        elem = names&.shift
+        elem&.at("./self::references[@normative = 'false']") ||
+          @log.add("Style", elem,
+                   "Final section must be (references) Bibliography")
+        names.empty? ||
+          @log.add("Style", elem,
+                   "There are sections after the final Bibliography")
+      end
     end
   end
 end
--- spec/metanorma/validate_spec.rb
+++ spec/metanorma/validate_spec.rb
@@ -5,6 +5,78 @@ RSpec.describe Metanorma::IEEE do
     FileUtils.rm_f "test.err"
   end
 
+  context "Warns of missing overview" do
+    it "Overview clause missing" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        text
+      INPUT
+
+      expect(File.read("test.err")).to include "Overview clause missing"
+      expect(File.read("test.err")).to include "Scope subclause missing"
+      expect(File.read("test.err")).to include "Word Usage subclause missing"
+    end
+
+    it "Overview clause not missing if supplied" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        == Overview
+      INPUT
+      expect(File.read("test.err")).not_to include "Overview clause missing"
+      expect(File.read("test.err")).to include "Scope subclause missing"
+      expect(File.read("test.err")).to include "Word Usage subclause missing"
+    end
+
+    it "Scope and Word usage clause not missing if supplied" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        == Overview
+
+        === Scope
+
+        === Word Usage
+      INPUT
+      expect(File.read("test.err")).not_to include "Overview clause missing"
+      expect(File.read("test.err")).not_to include "Scope subclause missing"
+      expect(File.read("test.err")).not_to include "Word Usage subclause missing"
+    end
+
+    it "Overview clause not missing in amendments" do
+      FileUtils.rm_f "test.err"
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: amendment
+
+        text
+      INPUT
+      expect(File.read("test.err")).not_to include "Overview clause missing"
+      expect(File.read("test.err")).not_to include "Scope subclause missing"
+      expect(File.read("test.err")).not_to include "Word Usage subclause missing"
+    end
+  end
+
   it "warns that title should match doctype" do
     Asciidoctor.convert(<<~"INPUT", *OPTIONS)
       = Fred
@@ -16,6 +88,100 @@ RSpec.describe Metanorma::IEEE do
 
     INPUT
     expect(File.exist?("test.err")).to be true
-    expect(File.read("test.err")).to include "Expected title to start as: Draft Trial-Use Recommended Practice"
+    expect(File.read("test.err")).to include "Expected title to start as: "\
+                                             "Draft Trial-Use Recommended Practice"
+  end
+
+  context "Warns of missing normative references" do
+    it "Normative references missing" do
+      FileUtils.rm_f "test.err"
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        text
+      INPUT
+      expect(File.read("test.err")).to include "Normative references missing"
+    end
+
+    it "Normative references not missing if supplied" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        [bibliography]
+        == Normative references
+      INPUT
+      expect(File.read("test.err"))
+        .not_to include "Normative references missing"
+    end
+
+    it "Normative references not missing in amendments" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: amendment
+
+        text
+      INPUT
+      expect(File.read("test.err"))
+        .not_to include "Normative references missing"
+    end
+  end
+
+  context "Warns of missing terms & definitions" do
+    it "Terms & definitions missing" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        text
+      INPUT
+      expect(File.read("test.err")).to include "Definitions missing"
+    end
+
+    it "Terms & definitions not missing if supplied" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: pizza
+
+        == Terms and definitions
+        === Term 1
+      INPUT
+      expect(File.read("test.err")).not_to include "Definitions missing"
+    end
+
+    it "Terms & definitions not missing in amendment" do
+      Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :no-isobib:
+        :doctype: amendment
+
+        text
+      INPUT
+      expect(File.read("test.err")).not_to include "Definitions missing"
+    end
   end
 end
