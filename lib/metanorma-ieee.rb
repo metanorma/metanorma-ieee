dc9e8091a28179ac7b42acd83fb3aab3bc162c67
Nick Nicholas <opoudjis@gmail.com>
2022-09-08 16:40:00 +1000

    rspec

--- lib/metanorma-ieee.rb
+++ lib/metanorma-ieee.rb
@@ -2,6 +2,7 @@ require "metanorma/ieee"
 require "asciidoctor"
 require "isodoc/ieee"
 require "html2doc/ieee"
+require "metanorma"
 
 if defined? Metanorma::Registry
   Metanorma::Registry.instance.register(Metanorma::IEEE::Processor)
