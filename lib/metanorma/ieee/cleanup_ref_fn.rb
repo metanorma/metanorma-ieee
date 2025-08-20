d48428ccbfe74c1e5fde9d6af7a1d75aef626abc
Nick Nicholas <opoudjis@gmail.com>
2025-08-21 00:39:34 +1000

    place availability footnote on references checking normative before non-normative references explicitly: https://github.com/metanorma/metanorma-ieee/issues/576

--- lib/metanorma/ieee/cleanup_ref_fn.rb
+++ lib/metanorma/ieee/cleanup_ref_fn.rb
@@ -5,8 +5,19 @@ module Metanorma
         "//references[@normative='true']/bibitem[not(note[@type = 'Availability'])] | "\
         "//references[@normative='false']/bibitem[not(note[@type = 'Availability'])]".freeze
 
+      def sorted_bibitem_no_avail(xmldoc)
+        # Get normative references first, maintaining their order
+        normative_bibitems = xmldoc.xpath("//references[@normative='true']/bibitem[not(note[@type = 'Availability'])]")
+        
+        # Get non-normative references second, maintaining their order  
+        non_normative_bibitems = xmldoc.xpath("//references[@normative='false']/bibitem[not(note[@type = 'Availability'])]")
+        
+        # Return concatenated array with normative first
+        normative_bibitems.to_a + non_normative_bibitems.to_a
+      end
+
       def withdrawn_note(xmldoc, provenance_notes)
-        xmldoc.xpath(BIBITEM_NO_AVAIL).each do |b|
+        sorted_bibitem_no_avail(xmldoc).each do |b|
           bib_pubs(b).include?(IEEE) or next
           b.at("./status/stage")&.text == "withdrawn" or next
           docid = b.at("./docidentifier[@type = 'IEEE'][not(@scope)]")
@@ -25,28 +36,29 @@ module Metanorma
       }.freeze
 
       def available_note(xmldoc, provenance_notes)
-        iso_iec_available_note(xmldoc, provenance_notes["iso-iec"], true, true)
-        iso_iec_available_note(xmldoc, provenance_notes["iso"], true, false)
-        iso_iec_available_note(xmldoc, provenance_notes["iec"], false, true)
-        itu_available_note(xmldoc, provenance_notes["itut"], true)
-        itu_available_note(xmldoc, provenance_notes["itur"], false)
-        nist_available_note(xmldoc, provenance_notes["fips"], true)
-        nist_available_note(xmldoc, provenance_notes["nist"], false)
-        ietf_available_note(xmldoc, provenance_notes["ietf"])
+        bibitems = sorted_bibitem_no_avail(xmldoc)
+        iso_iec_available_note(bibitems, provenance_notes["iso-iec"], true, true)
+        iso_iec_available_note(bibitems, provenance_notes["iso"], true, false)
+        iso_iec_available_note(bibitems, provenance_notes["iec"], false, true)
+        itu_available_note(bibitems, provenance_notes["itut"], true)
+        itu_available_note(bibitems, provenance_notes["itur"], false)
+        nist_available_note(bibitems, provenance_notes["fips"], true)
+        nist_available_note(bibitems, provenance_notes["nist"], false)
+        ietf_available_note(bibitems, provenance_notes["ietf"])
         AVAIL_PUBS.each do |k, v|
-          sdo_available_note(xmldoc, provenance_notes[k.to_s], v)
+          sdo_available_note(bibitems, provenance_notes[k.to_s], v)
         end
       end
 
-      def sdo_available_note(xmldoc, note, publisher)
-        ret = xmldoc.xpath(BIBITEM_NO_AVAIL).detect do |b|
+      def sdo_available_note(bibitems, note, publisher)
+        ret = bibitems.detect do |b|
           bib_pubs(b).include?(publisher)
         end
         insert_availability_note(ret, note)
       end
 
-      def iso_iec_available_note(xmldoc, note, iso, iec)
-        ret = xmldoc.xpath(BIBITEM_NO_AVAIL).detect do |b|
+      def iso_iec_available_note(bibitems, note, iso, iec)
+        ret = bibitems.detect do |b|
           pubs = bib_pubs(b)
           has_iec = pubs.include?("International Electrotechnical Commission")
           has_iso = pubs.include?("International Organization for Standardization")
@@ -56,15 +68,15 @@ module Metanorma
         insert_availability_note(ret, note)
       end
 
-      def ietf_available_note(xmldoc, note)
-        ret = xmldoc.xpath(BIBITEM_NO_AVAIL).detect do |b|
+      def ietf_available_note(bibitems, note)
+        ret = bibitems.detect do |b|
           b.at("./docidentifier[@type = 'IETF']")
         end
         insert_availability_note(ret, note)
       end
 
-      def itu_available_note(xmldoc, note, itu_t)
-        ret = xmldoc.xpath(BIBITEM_NO_AVAIL).detect do |b|
+      def itu_available_note(bibitems, note, itu_t)
+        ret = bibitems.detect do |b|
           has_itu_t = /^ITU-T/.match?(b.at("./docidentifier[@type = 'ITU']")&.text)
           bib_pubs(b).include?("International Telecommunication Union") &&
             !has_itu_t && !itu_t || (has_itu_t && itu_t)
@@ -72,8 +84,8 @@ module Metanorma
         insert_availability_note(ret, note)
       end
 
-      def nist_available_note(xmldoc, note, fips)
-        ret = xmldoc.xpath(BIBITEM_NO_AVAIL).detect do |b|
+      def nist_available_note(bibitems, note, fips)
+        ret = bibitems.detect do |b|
           id = b.at("./docidentifier[@type = 'NIST']")
           has_fips = /\bFIPS\b/.match?(id&.text)
           id && ((has_fips && !fips) || (!has_fips && fips))
