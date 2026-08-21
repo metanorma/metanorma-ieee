d8352b8b20ec8571db1f523fc296a9ad2d6ec5ce
Ronald Tse <ronald.tse@ribose.com>
2026-08-21 08:50:52 +0800

    OCP adoption: model tree + core flavor table registration (metanorma-core#18)

---
+++ lib/metanorma/ieee/document.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "metanorma/standoc"
+require "metanorma/iso/document/models"
+module Metanorma
+  module Ieee
+  end
+end
+
+module Metanorma
+  module Ieee::Document
+  end
+end
+
+module Metanorma
+  existing = defined?(Metanorma::IeeeDocument) && Metanorma::IeeeDocument
+  if !existing.equal?(Metanorma::Ieee::Document)
+    Metanorma.send(:remove_const, :IeeeDocument) if existing
+    IeeeDocument = Metanorma::Ieee::Document
+  end
+end
+
+# OCP adoption: ONE registration in the metanorma-core flavor table
+require "metanorma-core"
+require "metanorma/iso/html"
+
+Metanorma::Core::Flavors.register(Metanorma::Core::Flavor.new(
+  name: :ieee,
+  gem: "metanorma-ieee",
+  model_root: Metanorma::Ieee::Document::Root,
+  pubid_module: :"Pubid::Ieee",
+  renderers: { html: Metanorma::Iso::Html::Renderer },
+))
